79109f67964a6ef1776e7362c4b1d2778dd28391
alt610 <alfintaufiqurrahman@gmail.com>
2017-12-06 20:50:40 +0700

    Version 1.2
    ChangeLog:
    -Hasil hitung BMI dan rekomendasi makanan ditampilkan di halaman lain di luar halaman input
    -Tombol Home pada halaman hasil untuk kembali ke halaman awal

--- app/src/main/java/com/example/gpramasita/aplikasikebugaran/MainActivity.java
+++ app/src/main/java/com/example/gpramasita/aplikasikebugaran/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.gpramasita.aplikasikebugaran;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText height;
     private EditText weight;
-    private TextView result;
+    private Button hitung;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,45 +20,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         height = (EditText) findViewById(R.id.height);
         weight = (EditText) findViewById(R.id.weight);
-        result = (TextView) findViewById(R.id.result);
+        hitung = (Button) findViewById(R.id.calc);
+
+        hitung.setOnClickListener(this);
     }
 
-    public void calculateBMI(View v) {
-        String heightStr = height.getText().toString();
-        String weightStr = weight.getText().toString();
 
-        if (heightStr != null && !"".equals(heightStr)
-                && weightStr != null  &&  !"".equals(weightStr)) {
-            float heightValue = Float.parseFloat(heightStr) / 100;
-            float weightValue = Float.parseFloat(weightStr);
 
-            float bmi = weightValue / (heightValue * heightValue);
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(this, OutputBMI.class);
 
-            displayBMI(bmi);
-        }
-    }
+        i.putExtra("height", height.getText().toString());
+        i.putExtra("weight", weight.getText().toString());
 
-    private void displayBMI(float bmi) {
-        String bmiLabel = "";
-
-        if (Float.compare(bmi, 15f) <= 0) {
-            bmiLabel = "Sangat Sangat Kurus \n\n Rekomendasi: Perbanyak konsumsi protein hewani dan serat";
-        } else if (Float.compare(bmi, 15f) > 0  &&  Float.compare(bmi, 16f) <= 0) {
-            bmiLabel = "Sangat Kurus \n\n Rekomendasi: Perbanyak konsumsi protein hewani dan serat";
-        } else if (Float.compare(bmi, 16f) > 0  &&  Float.compare(bmi, 18.5f) <= 0) {
-            bmiLabel = "Kurus \n\n Rekomendasi: Perbanyak konsumsi protein hewani dan serat";
-        } else if (Float.compare(bmi, 18.5f) > 0  &&  Float.compare(bmi, 25f) <= 0) {
-            bmiLabel = "Normal \n\n Rekomendasi: Pertahankan konsumsi protein hewani dan serat";
-        } else if (Float.compare(bmi, 25f) > 0  &&  Float.compare(bmi, 30f) <= 0) {
-            bmiLabel = "Gemuk \n\n Rekomendasi: Kurangi konsumsi protein hewani dan perbanyak serat";
-        } else if (Float.compare(bmi, 30f) > 0  &&  Float.compare(bmi, 35f) <= 0) {
-            bmiLabel = "Obesitas Tingkat I \n\n Rekomendasi: Kurangi konsumsi protein hewani dan perbanyak serat";
-        } else if (Float.compare(bmi, 35f) > 0  &&  Float.compare(bmi, 40f) <= 0) {
-            bmiLabel = "Obesitas Tingkat II \n\n Rekomendasi: Kurangi konsumsi protein hewani dan perbanyak serat";
-        } else {
-            bmiLabel = "Obesitas Tingkat III \n\n Rekomendasi: Kurangi konsumsi protein hewani dan perbanyak serat";
-        }
-        bmiLabel = bmi+ "\n\n" + bmiLabel;
-        result.setText(bmiLabel);
+        startActivity(i);
     }
 }
