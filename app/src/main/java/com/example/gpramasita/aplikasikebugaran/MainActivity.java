2ad210bafd5b00280bb2608200075bc43bb68dd3
alt610 <alfintaufiqurrahman@gmail.com>
2017-12-06 17:52:09 +0700

    Version 1.0
    -[APP-1-01] Menghitung BMI

--- app/src/main/java/com/example/gpramasita/aplikasikebugaran/MainActivity.java
+++ app/src/main/java/com/example/gpramasita/aplikasikebugaran/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.gpramasita.aplikasikebugaran;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText height;
+    private EditText weight;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        height = (EditText) findViewById(R.id.height);
+        weight = (EditText) findViewById(R.id.weight);
+        result = (TextView) findViewById(R.id.result);
+    }
+
+    public void calculateBMI(View v) {
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+        if (heightStr != null && !"".equals(heightStr)
+                && weightStr != null  &&  !"".equals(weightStr)) {
+            float heightValue = Float.parseFloat(heightStr) / 100;
+            float weightValue = Float.parseFloat(weightStr);
+
+            float bmi = weightValue / (heightValue * heightValue);
+
+            displayBMI(bmi);
+        }
+    }
+
+    private void displayBMI(float bmi) {
+        String bmiLabel = "";
+
+        if (Float.compare(bmi, 15f) <= 0) {
+            bmiLabel = "sangat sangat kurus";
+        } else if (Float.compare(bmi, 15f) > 0  &&  Float.compare(bmi, 16f) <= 0) {
+            bmiLabel = "sangat kurus";
+        } else if (Float.compare(bmi, 16f) > 0  &&  Float.compare(bmi, 18.5f) <= 0) {
+            bmiLabel = "kurus";
+        } else if (Float.compare(bmi, 18.5f) > 0  &&  Float.compare(bmi, 25f) <= 0) {
+            bmiLabel = "normal";
+        } else if (Float.compare(bmi, 25f) > 0  &&  Float.compare(bmi, 30f) <= 0) {
+            bmiLabel = "gemuk";
+        } else if (Float.compare(bmi, 30f) > 0  &&  Float.compare(bmi, 35f) <= 0) {
+            bmiLabel = "obesitas tingkat I";
+        } else if (Float.compare(bmi, 35f) > 0  &&  Float.compare(bmi, 40f) <= 0) {
+            bmiLabel = "obesitas tingkat II";
+        } else {
+            bmiLabel = "obesitas tingkat III";
+        }
+        bmiLabel = bmi+ "\n\n" + bmiLabel;
+        result.setText(bmiLabel);
     }
 }
